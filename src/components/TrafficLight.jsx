6190893de50a5ce1075a39758fde5f010e460608
Marie <marievstewart@gmail.com>
2023-03-09 01:34:56 +0000

    Traffic light with a touch of pizazz

---
+++ src/components/TrafficLight.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+
+export function TrafficLight () {
+    const [ color, setColor] = useState("");
+    return (
+        <div className="container">
+    <div className="trafficLight">
+        <div className="stem"></div>
+            <div className="box">
+            <div className={"redLight" + (color === "red" ? " on" : "")} 
+            onClick={() => {
+                setColor("red")
+            }}></div>
+            <div className={"yellowLight" + (color === "yellow" ? " on" : "")}  onClick={() => {
+                setColor("yellow")
+            }}></div>
+            <div className={"greenLight" + (color === "green" ? " on" : "")}  onClick={() => {
+                setColor("green")
+            }}></div>
+    </div>
+        </div>
+            </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
-import { Footer } from "@/components/Footer";
-import { Welcome } from "@/components/Welcome";
+import { TrafficLight } from "@/components/TrafficLight";
 
 export default function Home() {
   return (
-    <div className="container">
-      <Welcome />
-      <Footer />
+    <div className="container" style={{backgroundImage:`url("https://picsum.photos/882/1000")`}}>
+      <TrafficLight />
     </div>
   );
 }
